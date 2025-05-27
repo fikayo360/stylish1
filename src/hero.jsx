94c0f847e9f18d81b0ce126914fa31bb549f7db8
fikayo <fikayoadele@gmail.com>
2025-05-27 12:02:16 +0100

    final

--- src/hero.jsx
+++ src/hero.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useRef, useState } from 'react';
-import './HeroSection.css';
+import './heroSection.css';
 
 const Hero = () => {
     const [isNavActive, setIsNavActive] = useState(false);
